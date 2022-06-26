f40c03283144c336b15e94b22aaba7201f7126c5
Yohan <yohan@inflab.com>
2022-06-26 12:37:34 +0900

    feat: Token 생성

---
+++ src/token/token.rs
@@ -0,0 +1,24 @@
+pub enum Token {
+    ILLEGAL,
+    EOF,
+
+    // 식별자 + 리터럴
+    IDENT,
+    INT,
+
+    // 연산자
+    ASSIGN,
+    PLUS,
+
+    // 구분자
+    COMMA,
+    SEMICOLON,
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+
+    // 예약어
+    FUNCTION,
+    LET,
+}
