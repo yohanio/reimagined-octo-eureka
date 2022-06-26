15807408f137501aa63aa98ec4c203943aa37b3b
Yohan <yohan@inflab.com>
2022-06-26 16:32:45 +0900

    feat: 간단한 REPL 구현

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use rustyline::Editor;
+use rustyline::error::ReadlineError;
+use interpreter_in_rs::lexer::lexer::Lexer;
+use interpreter_in_rs::token::token::Token;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rl = Editor::<()>::new();
+
+    loop {
+        match rl.readline(">> ") {
+            Ok(line) => {
+                rl.add_history_entry(&line);
+
+                let mut lexer = Lexer::new(&line);
+
+                loop {
+                    let tok = lexer.next_token();
+                    if tok == Token::EOF {
+                        break;
+                    }
+                    println!("{:?}", tok);
+                }
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("REPL : Bye!");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!();
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+            }
+        }
+    }
 }
